ff500a3ef616f9888aefa8a029d6cb4a1f845491
Toba0z <tobgabby@aol.com>
2024-03-17 00:33:00 -0700

    fixing item routes for edit item

--- routes/itemsRoutes.js
+++ routes/itemsRoutes.js
@@ -31,26 +31,26 @@ router
     }
   })
 
-// router.route('/:id')
-// //get one inventory item information 
-// .get(async (req, res) =>  {
-//     try { const id =req.params.id;
-//     const item = await knex("inventories").where({id:id}).first();
-//     if(!item){
-//       return res.status(404).json({error: "Item not found"})
-//     }
-//     res.status(200).json(item);
+router.route('/:id')
+//get one inventory item information 
+.get(async (req, res) =>  {
+    try { const id =req.params.id;
+    const item = await knex("inventories").where({id:id}).first();
+    if(!item){
+      return res.status(404).json({error: "Item not found"})
+    }
+    res.status(200).json(item);
         
-//     } catch (error) {
-//         console.log('This is the error:', error)
-//         res.status(500).json({ error: "Internal server error" });
+    } catch (error) {
+        console.log('This is the error:', error)
+        res.status(500).json({ error: "Internal server error" });
 
-//     }
-//   })
+    }
+  })
 
 
 //get one inventory item information 
-router.route('/:id')
+// router.route('/:id')
   .get(async (req, res) => {
     try {
       const id = req.params.id;
